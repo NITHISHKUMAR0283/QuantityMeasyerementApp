34ba155ca669f13c4d7d4d92eddb18e39f800aec
NithishkumarA <nk0283@srmist.edu.in>
2026-05-05 21:40:42 +0530

    initial commit

---
+++ QuantityMeasurementApp.java
@@ -0,0 +1,38 @@
+public class QuantityMeasurementApp {
+
+    static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        public double getValue() {
+            return value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || this.getClass() != obj.getClass()) return false;
+            Feet other = (Feet) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
+
+    public static void main(String[] args) {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+        Feet f3 = new Feet(2.0);
+
+        System.out.println(f1.equals(f2));
+        System.out.println(f1.equals(f3));
+        System.out.println(f1.equals(null));
+        System.out.println(f1.equals(f1));
+    }
+}
